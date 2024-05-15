508175a78d6ce91348858cb37f0295eee04a2589
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2024-05-15 21:34:14 +0000

    start landing page

---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Jumbotron = () => {
+    return (
+        <div className="container card mb-3 bg-light col-12">
+            <div className="card-body">
+                <h1 className="card-title">Card title</h1>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" className="btn btn-primary">Button</a>
+            </div>
+        </div>
+    );
+
+};
+
+export default Jumbotron
---
+++ src/js/component/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-black">
+            <div className="container-fluid">
+                <a className="navbar-brand text-light" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item ">
+                            <a className="nav-link text-secondary" href="#">Features</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-secondary" href="#">Pricing</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-secondary" href="#">Pricing</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    );
+
+};
+
+export default Navbar
